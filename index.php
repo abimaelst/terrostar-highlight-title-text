5d6d283ed3dd9e5da52d34e66c842b41e4f6a351
Abimael Tavares <tavares@abimael.dev.br>
2024-02-07 12:06:37 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+# To us, this is not a job, It's a lifestyle. A busy one.
---
+++ terrostar_highlight_title_text.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Terrostar Highlight Title Text is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Terrostar Highlight Title Text is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Terrostar Highlight Title Text. If not, see http://www.wordpress.org/terrostar_highlight_title_text
+*/
+
+
+/**
+* Plugin Name: Terrostar Highlight Title Text
+* Plugin URI: https://github.com/abimaelst
+* Description: Allows users to highlight words or phrases in post and page titles.
+* Version: 0.1
+* Author: Abimael Tavares
+* Author URI: https://github.com/abimaelst
+**/
