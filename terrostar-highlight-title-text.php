8ac92ba6a9e5c4c75c5ad493e72145a491c6b770
Abimael Tavares <tavares@abimael.dev.br>
2024-02-07 12:20:54 -0300

    creating initial class to load the plugin

--- terrostar-highlight-title-text.php
+++ terrostar-highlight-title-text.php
@@ -17,13 +17,57 @@ along with Terrostar Highlight Title Text. If not, see http://www.wordpress.org/
 
 
 /**
-* Plugin Name: Terrostar Highlight Title Text
-* Plugin URI: https://github.com/abimaelst
-* Description: Allows users to highlight words or phrases in post and page titles.
-* Version: 0.1
-* Requires at least: 6.2
-* License: GPL v2 or later
-* Author: Abimael Tavares
-* Text Domain: terrostar-highlight-title-text
-* Author URI: https://github.com/abimaelst
-**/
+ * Plugin Name: Terrostar Highlight Title Text
+ * Plugin URI: https://github.com/abimaelst
+ * Description: Allows users to highlight words or phrases in post and page titles.
+ * Version: 1.0
+ * Requires at least: 6.2
+ * License: GPL v2 or later
+ * Author: Abimael Tavares
+ * Text Domain: terrostar-highlight-title-text
+ * Author URI: https://github.com/abimaelst/terrostar-highlight-title-text
+
+ **/
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if (!class_exists('HL_Title_Text')) {
+    class HL_Title_Text
+    {
+        function __construct()
+        {
+            $this->define_constants();
+        }
+
+        public function define_constants()
+        {
+            define('HL_TITLE_TEXT_PATH', plugin_dir_path(__FILE__));
+            define('HL_TITLE_TEXT_URL', plugin_dir_url(__FILE__));
+            define('HL_TITLE_TEXT_VERSION', '1.0.0');
+        }
+
+        public static function activate()
+        {
+            update_option('rewrite_rules', '');
+        }
+
+        public static function deactivate()
+        {
+            flush_rewrite_rules();
+            unregister_post_type('mv-slider');
+        }
+
+        public static function uninstall()
+        {
+        }
+    }
+
+    if (class_exists('HL_Title_Text')) {
+        register_activation_hook(__FILE__, ['HL_Title_Text' => 'activate']);
+        register_deactivation_hook(__FILE__, ['HL_Title_Text' => 'deactivate']);
+        register_uninstall_hook(__FILE__, ['HL_Title_Text' => 'uninstall']);
+        $mv_slider = new HL_Title_Text();
+    }
+}
